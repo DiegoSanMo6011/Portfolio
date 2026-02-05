9db8704e13d862ca5c1c021aadd1065b845919ee
Diego Gerardo Sanchez Moreno <Diegogerardosanchez3@gmail.com>
2026-02-04 18:16:24 -0600

    Add ATS robotics and software CVs

--- cv/build.sh
+++ cv/build.sh
@@ -8,5 +8,10 @@ OUT_DIR="$ROOT_DIR/public/assets/docs"
 mkdir -p "$OUT_DIR"
 
 pdflatex -interaction=nonstopmode -output-directory "$OUT_DIR" "$CV_DIR/diego_sanchez_cv.tex" >/dev/null
-
 mv -f "$OUT_DIR/diego_sanchez_cv.pdf" "$OUT_DIR/Diego-Gerardo-Sanchez-Moreno-CV.pdf"
+
+pdflatex -interaction=nonstopmode -output-directory "$OUT_DIR" "$CV_DIR/diego_sanchez_cv_robotics.tex" >/dev/null
+mv -f "$OUT_DIR/diego_sanchez_cv_robotics.pdf" "$OUT_DIR/Diego-Gerardo-Sanchez-Moreno-CV-Robotics.pdf"
+
+pdflatex -interaction=nonstopmode -output-directory "$OUT_DIR" "$CV_DIR/diego_sanchez_cv_software.tex" >/dev/null
+mv -f "$OUT_DIR/diego_sanchez_cv_software.pdf" "$OUT_DIR/Diego-Gerardo-Sanchez-Moreno-CV-Software.pdf"
